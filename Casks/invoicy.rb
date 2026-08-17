1d4afb09d4bc42924877eb6bf55125bc77c1b200
Ice <17621507+ice-chillios@users.noreply.github.com>
2026-08-17 13:10:26 +0200

    chore(invoicy): update cask to 0.10.1 (#1)

--- Casks/invoicy.rb
+++ Casks/invoicy.rb
@@ -1,13 +1,22 @@
-cask "invoicy" do
-  version "0.9.9"
-  sha256 :no_check
+# frozen_string_literal: true
 
-  url "https://releases.r5n.dev/Invoicy-macos-arm64.dmg"
+cask "invoicy" do
+  version "0.10.1"
+  sha256 "7d0aeb5b006ea8510c058d1a2b1125b6af069b09befee7e812f3ef5675cbb49a"
+
+  url "https://releases.r5n.dev/invoicy/Invoicy-#{version}-macos-arm64.dmg"
   name "Invoicy"
   desc "TBA"
   homepage "https://r5n.dev/"
 
-  depends_on macos: ">= :ventura"
+  livecheck do
+    url "https://releases.r5n.dev/invoicy/stable-macos-arm64-update.json"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  depends_on macos: :ventura
 
   app "Invoicy.app"
 
@@ -15,11 +24,4 @@ cask "invoicy" do
     "~/Library/Application Support/dev.r5n.invoicy",
     "~/Library/Preferences/dev.r5n.invoicy.plist",
   ]
-
-  livecheck do
-    url "https://releases.r5n.dev/Invoicy-macos-arm64.json"
-    strategy :json do |json|
-      json["version"]
-    end
-  end
 end
